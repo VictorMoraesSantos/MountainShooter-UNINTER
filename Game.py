0a3990c093e80637ee17f992612e4feeef0e72f8
Victor Moraes <victormoraes.2002@hotmail.com>
2025-03-04 21:03:49 -0300

    connecting menu and game classes

---
+++ Game.py
@@ -0,0 +1,14 @@
+import pygame
+from Menu import Menu
+
+class Game:
+    def __init__(self):
+        pygame.init()
+        self.window = pygame.display.set_mode(size=(600, 480))
+
+    def run(self):
+        while True:
+            menu = Menu(self.window)
+            menu.run()
+            pass
+
