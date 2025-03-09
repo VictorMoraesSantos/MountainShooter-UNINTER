fdad88164a581be1f4545a1f65854eac02f22fa8
Victor Moraes <victormoraes.2002@hotmail.com>
2025-03-09 18:08:06 -0300

    level 2 implementation

--- Const.py
+++ Const.py
@@ -9,7 +9,7 @@ C_CYAN = (0, 128, 128)
 
 # E
 EVENT_ENEMY = pygame.USEREVENT + 1
-
+EVENT_TIMEOUT = pygame.USEREVENT + 2
 ENTITY_SPEED = {
     "Level1Bg0": 0,
     "Level1Bg1": 1,
@@ -18,6 +18,11 @@ ENTITY_SPEED = {
     "Level1Bg4": 4,
     "Level1Bg5": 5,
     "Level1Bg6": 6,
+    "Level2Bg0": 0,
+    "Level2Bg1": 1,
+    "Level2Bg2": 2,
+    "Level2Bg3": 3,
+    "Level2Bg4": 4,
     "Player1": 3,
     "Player1Shoot": 1,
     "Player2": 3,
@@ -34,6 +39,11 @@ ENTITY_HEALTH = {
     'Level1Bg2': 999,
     'Level1Bg3': 999,
     'Level1Bg4': 999,
+    'Level2Bg0': 999,
+    'Level2Bg1': 999,
+    'Level2Bg2': 999,
+    'Level2Bg3': 999,
+    'Level2Bg4': 999,
     'Level1Bg5': 999,
     'Level1Bg6': 999,
     'Player1': 300,
@@ -60,9 +70,9 @@ ENTITY_DAMAGE = {
     'Level2Bg3': 0,
     'Level2Bg4': 0,
     'Player1': 1,
-    'Player1Shot': 25,
+    'Player1Shoot': 25,
     'Player2': 1,
-    'Player2Shot': 20,
+    'Player2Shoot': 20,
     'Enemy1': 1,
     'Enemy1Shoot': 20,
     'Enemy2': 1,
@@ -83,13 +93,13 @@ ENTITY_SCORE = {
     'Level2Bg3': 0,
     'Level2Bg4': 0,
     'Player1': 0,
-    'Player1Shot': 0,
+    'Player1Shoot': 0,
     'Player2': 0,
-    'Player2Shot': 0,
+    'Player2Shoot': 0,
     'Enemy1': 100,
-    'Enemy1Shot': 0,
+    'Enemy1Shoot': 0,
     'Enemy2': 125,
-    'Enemy2Shot': 0,
+    'Enemy2Shoot': 0,
 }
 
 ENTITY_SHOOT_DELAY = {
@@ -115,7 +125,11 @@ PLAYER_KEY_RIGHT = {'Player1': pygame.K_RIGHT, 'Player2': pygame.K_d}
 PLAYER_KEY_SHOOT = {'Player1': pygame.K_RCTRL, 'Player2': pygame.K_LCTRL}
 
 # S
-SPAWN_TIME = 4000
+SPAWN_TIME = 3000
+
+# T
+TIMEOUT_STEP = 100
+TIMEOUT_LEVEL = 20000
 
 # W
 WIN_WIDTH = 576
